d06a84c3321d731cf32b2a8d3a6a6fc14b2f2dfb
andreazunino <108498948+andreazunino@users.noreply.github.com>
2024-11-10 12:16:05 -0300

    se acomodan archivos

---
+++ Instiform/app/frontend/templates_c/f2e751b4b7d63229f1a6f1ed87c4a36bf493a1ac_0.file_altaEstudiante.tpl.php
@@ -0,0 +1,147 @@
+<?php
+/* Smarty version 5.4.0, created on 2024-11-10 15:39:03
+  from 'file:templates/altaEstudiante.tpl' */
+
+/* @var \Smarty\Template $_smarty_tpl */
+if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
+  'version' => '5.4.0',
+  'unifunc' => 'content_6730c5872a1fc6_74110525',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'f2e751b4b7d63229f1a6f1ed87c4a36bf493a1ac' => 
+    array (
+      0 => 'templates/altaEstudiante.tpl',
+      1 => 1728339182,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:templates/head.tpl' => 1,
+  ),
+))) {
+function content_6730c5872a1fc6_74110525 (\Smarty\Template $_smarty_tpl) {
+$_smarty_current_dir = 'C:\\xampp\\htdocs\\InstiSmarty\\Instiform\\app\\frontend\\templates';
+?><!DOCTYPE html>
+<html lang="es">
+<?php $_smarty_tpl->renderSubTemplate('file:templates/head.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), (int) 0, $_smarty_current_dir);
+?>
+<body>
+<style>
+    body {
+        background: url('fondo.avif') no-repeat center center fixed;
+        background-size: cover;
+        background: linear-gradient(to bottom, #a1c4fd, #c2e9fb); /* Degradado de fondo */
+        min-height: 100vh;
+        margin: 0;
+        font-family: 'Arial', sans-serif;
+    }
+    .logo-small {
+        max-width: 50px;
+        margin-top: 10px;
+    }
+    .navbar {
+        margin-bottom: 20px;
+    }
+    .btn-logout {
+        background-color: #d33f4d;
+        color: #ffffff;
+        border: none;
+        padding: 10px 20px;
+        font-size: 14px;
+        font-weight: bold;
+        text-transform: uppercase;
+        border-radius: 50px;
+        transition: background-color 0.3s ease;
+        position: absolute;
+        top: 20px;
+        right: 20px;
+    }
+    .btn-logout:hover {
+        background-color: #63597a;
+    }
+    .btn-custom {
+        background-color: #4a90e2;
+        color: #ffffff;
+        border: none;
+        padding: 15px 30px;
+        font-size: 18px;
+        font-weight: bold;
+        text-transform: uppercase;
+        border-radius: 50px;
+        transition: background-color 0.3s ease;
+    }
+</style>
+
+<button class="btn btn-logout" onclick="window.location.href='index.php'">Cerrar sesión</button>
+
+<div class="container-fluid text-center welcome-section">
+    <img src="Logo instiform.png" alt="Logo de Instiform" class="img-fluid logo-small">
+    <h1 class="welcome-heading">Dar de Alta Estudiantes</h1>
+</div>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mx-auto d-flex">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="menuAdministrador.php" id="navbarDropdownMenuLink" role="button" aria-haspopup="true" aria-expanded="false">
+                    Volver al Menú Administrador
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container text-center">
+    
+    
+    <!-- Mostrar mensajes de éxito o error -->
+    <?php if ($_smarty_tpl->getValue('mensaje')) {?>
+        <div class="alert alert-<?php echo $_smarty_tpl->getValue('mensaje_tipo');?>
+ alert-dismissible fade show" role="alert">
+           <?php echo $_smarty_tpl->getValue('mensaje');?>
+
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+    <?php }?>
+
+
+    <form action="altaEstudiante.php" method="POST">
+        <div class="form-group">
+            <label for="dni">DNI:</label>
+            <input type="text" class="form-control" id="dni" name="dni" required pattern="\d+" title="Solo se permiten números">
+        </div>
+        <div class="form-group">
+            <label for="nombre">Nombre:</label>
+            <input type="text" class="form-control" id="nombre" name="nombre" required>
+        </div>
+        <div class="form-group">
+            <label for="apellido">Apellido:</label>
+            <input type="text" class="form-control" id="apellido" name="apellido" required>
+        </div>
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" class="form-control" id="email" name="email" required>
+        </div>
+        <button type="submit" class="btn btn-custom">Dar de Alta</button>
+    </form>
+    
+<?php echo '<script'; ?>
+ src="https://code.jquery.com/jquery-3.5.1.slim.min.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"><?php echo '</script'; ?>
+>
+</body>
+</html>
+<?php }
+}
